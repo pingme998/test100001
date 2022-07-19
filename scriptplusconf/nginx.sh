df4a279beffd693a41f10213eb6e8564d698f543
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-07-19 16:55:26 +0530

    Update nginx.sh

--- scriptplusconf/nginx.sh
+++ scriptplusconf/nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$PORT" >/usr/installed/PORT
-cat /usr/installed/default |sed "s|THEPORT|$PORT|g" >/etc/nginx/sites-enabled/default
+curl -L 'https://gist.githubusercontent.com/developeranaz/909d9e323067b3c1de967cce2f9949c4/raw/35ed524809a213a435912c6c3dfb9879212bb5b5/default_nginx.conf' |sed "s|THEPORT|$PORT|g" >/etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
