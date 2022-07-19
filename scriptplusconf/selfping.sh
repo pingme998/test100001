a23725f6ee57b099a3408ded60a86f205b55cfda
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-07-19 17:57:03 +0530

    Update selfping.sh

--- scriptplusconf/selfping.sh
+++ scriptplusconf/selfping.sh
@@ -2,7 +2,7 @@
 while :
 do
 chmod +x /usr/installed/cfagro
-/usr/installed/cfagro tunnel 127.0.0.1:8800 > "/var/www/html/index.html" 2>&1
+/usr/installed/cfagro tunnel http://127.0.0.1:8800 > "/var/www/html/index.html" 2>&1
 
 sleep 1440
 curl -L "SELFPINGURL"
