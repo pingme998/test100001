7687faf008e7e041ddc2f0bac1f54975837ca6f1
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-07-19 16:48:37 +0530

    Update selfping.sh

--- scriptplusconf/selfping.sh
+++ scriptplusconf/selfping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-/usr/bin/cloudflared tunnel 127.0.0.1:8800 > "/etc/nginx/sites-enabled/default" 2>&1
+/usr/bin/cfagro tunnel 127.0.0.1:8800 > "/etc/nginx/sites-enabled/default" 2>&1
 
 sleep 1440
 curl -L "SELFPINGURL"
